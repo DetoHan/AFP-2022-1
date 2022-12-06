1567fa798be77916d68c960b137cc7a7634b9367
ocbtomi36 <118051723+ocbtomi36@users.noreply.github.com>
2022-12-06 10:15:31 +0100

    Update header.inc.php
    weboldal címének a kialakítása

---
+++ AFP/resources/views/inc/header.inc.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php if(!empty($webCim)): ?>
+        <title><?php echo $webCim ?></title>
+    <?php else: ?>
+        <title>A webolal címe nincs beállítva ! Pótold</title>
+    <?php endif; ?>
+</head>
+<body>
+    
+</body>
+</html>
